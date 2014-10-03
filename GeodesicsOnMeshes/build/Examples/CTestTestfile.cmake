00350dbee38228dc9b28aa3d2d0d470ec14a9080
Elizabeth Vargas <liz2102@gmail.com>
2014-10-03 11:38:25 +0100

    Issue #1 Geodesic distance using VTK http://www.vtkjournal.org/browse/publication/891 and conversion VTK-PCL https://github.com/daviddoria/VTK_PCL_Conversions

---
+++ GeodesicsOnMeshes/build/Examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/VIBOT/Semester III/Robotics Project/GeodesicsOnMeshes/Examples
+# Build directory: E:/VIBOT/Semester III/Robotics Project/GeodesicsOnMeshes/build/Examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
